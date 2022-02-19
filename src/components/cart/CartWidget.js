9f8689f4f2f2164fc460d64497856a7f795e53a2
Mariela Pages <mai.pages5@gmail.com>
2022-02-18 21:40:39 -0300

    Cart number, totals and message for no items

--- src/components/cart/CartWidget.js
+++ src/components/cart/CartWidget.js
@@ -1,14 +1,39 @@
 import cart from "./cart-img/cart-icon.png";
 import "./CartWidget.css";
+import { Link } from 'react-router-dom'
+import React, { useContext } from "react"
+import { ProductsContext } from "../../CartContext/CartContext"
 
 const CartWidget = () => {
-    return(
-        <div>
-            <a href="" className="cart">
-              <img src={cart} alt="Cart logo" />
-            </a>
-        </div>
-    )
+
+    //Calculo el total variable de items
+    const [addedProducts] = useContext(ProductsContext);
+
+    const itemsPerProduct = []
+
+    let itemsAmount = 0
+
+    addedProducts.forEach(product => {
+        itemsPerProduct.push(product.quantity)
+    });
+
+    for (let i=0; i < itemsPerProduct.length; i++){
+        itemsAmount = itemsAmount + itemsPerProduct[i]; 
+    }
+
+    if(itemsAmount !== 0){
+        return(
+            <div>
+                <Link to="/cart" className="cart">
+                  <img src={cart} alt="Cart logo" />
+                </Link>
+                <p className="cartNumberProds">{itemsAmount}</p>
+            </div>
+        )
+    }
+    else{
+        return(<div className="nothing"></div>)
+    }
 }
 
 export default CartWidget;
--- src/views/Cart/Cart.js
+++ src/views/Cart/Cart.js
@@ -1,6 +1,7 @@
-import React, { useState, useContext} from "react"
+import React, { useContext } from "react"
 import { ProductsContext } from '../../CartContext/CartContext'
 import '../Cart/Cart.css'
+import { Link } from "react-router-dom"
 
 const Cart = () => {
     const [addedProducts, setAddedProducts] = useContext(ProductsContext);
@@ -14,34 +15,62 @@ const Cart = () => {
         e.target.setAttribute("style", "display:None;")
     }
 
-    return(
-        <>
-            <div className="container">
-                <div className="row">
-                    {(addedProducts.map(product => {
-                        return(
-                            <div key={product.id} className="col-sm-12 col-md-6 col-lg-4 col-xxl-3">
-                                <div className="card cardContainer" style={{width: '18rem'}}>
-                                    <div className="imageContainer">
-                                        <img src={product.image} className="card-img-top" alt={product.title} />
+    //Calculo el total de los productos primero pasando los subtotales a un array y luego sumandolos
+    const totalPerProduct = [];
+    
+    addedProducts.forEach(product => {
+        let totalPrice = product.price * product.quantity;
+        totalPerProduct.push(totalPrice)
+    })
+    
+    let total = 0;
+
+    for (let i=0; i < totalPerProduct.length; i++){
+        total = total + totalPerProduct[i];
+    }
+
+    if(total === 0){
+        return(
+            <div className="messageIfNoItems">
+                <p className="noItems">There are no items selected</p>
+                <Link to="/" className="backHomeSign">Go back to Home</Link>
+            </div>
+        )
+    }
+    else{
+        return(
+            <>
+                <div className="container">
+                    <div className="row">
+                        {(addedProducts.map(product => {
+                            return(
+                                <div key={product.id} className="col-sm-12 col-md-6 col-lg-4 col-xxl-3">
+                                    <div className="card cardContainer" style={{width: '18rem'}}>
+                                        <div className="imageContainer">
+                                            <img src={product.image} className="card-img-top" alt={product.title} />
+                                        </div>
+                                        <div className="card-body cardBodyContainer">
+                                            <p className="card-text text-center">{(product.title).toUpperCase()}</p>
+                                            <p className="card-text text-center itemPrice">usd {product.price}</p> 
+                                            <p className="card-text text-center">{(product.quantity)}</p>
+                                        </div>
+                                        <button className="deleteButton1" onClick={() => deleteProduct(product.title)}>Delete</button>
                                     </div>
-                                    <div className="card-body cardBodyContainer">
-                                        <p className="card-text text-center">{(product.title).toUpperCase()}</p>
-                                        <p className="card-text text-center itemPrice">usd {product.price}</p> 
-                                        <p className="card-text text-center">{(product.quantity)}</p>
-                                    </div>
-                                    <button className="deleteButton1" onClick={() => deleteProduct(product.title)}>Delete</button>
                                 </div>
-                            </div>
-                        )
-                    }))}
+                            )
+                        }))}
+                    </div>
                 </div>
-            </div>
-            <div className="d-flex justify-content-center deleteButton2">
-                <button onClick={clearAll}>Delete all</button>
-            </div>
-        </>
-    )
+                <div className='total'>
+                    <p>Total: {total}</p>
+                </div>
+                <div className="d-flex justify-content-center deleteButton2">
+                    <button onClick={clearAll}>Delete all</button>
+                </div>
+            </>
+        )
+    }
+
 }
 
 export default Cart
